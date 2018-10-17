ab6a906758cb889d2b2819096e084887c0559a12
Anusha <anushanadigm@gmail.com>
2018-10-17 10:25:22 +0530

    counter app using states part 1

--- IndecisionApp/src/others/counter-app.js
+++ IndecisionApp/src/others/counter-app.js
@@ -1,6 +1,12 @@
 //counter app
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
   handleAddOne() {
     alert("+1");
   }
